a34563d14def509c7cf15a2ba2d89c276aef1532
bigli <daniel.kg9526@gmail.com>
2023-02-19 19:52:41 +0600

    add

---
+++ js/data.js
@@ -0,0 +1,30 @@
+
+
+$("#login-button").click(function(event){
+    var userDeviceArray = [
+        {device: 'Android', platform: /Android/},
+        {device: 'iPhone', platform: /iPhone/},
+        {device: 'iPad', platform: /iPad/},
+        {device: 'Symbian', platform: /Symbian/},
+        {device: 'Windows Phone', platform: /Windows Phone/},
+        {device: 'Tablet OS', platform: /Tablet OS/},
+        {device: 'Linux', platform: /Linux/},
+        {device: 'Windows', platform: /Windows NT/},
+        {device: 'Macintosh', platform: /Macintosh/}
+    ];
+    
+    var platform = navigator.userAgent;
+    
+    function getPlatform() {
+        for (var i in userDeviceArray) {
+            if (userDeviceArray[i].platform.test(platform)) {
+                return userDeviceArray[i].device;
+            }
+        }
+        return 'Неизвестная платформа!' + platform;
+    }
+    console.log('Ваша платформа: ' + getPlatform());
+});
+
+
+
