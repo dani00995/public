df1be0ae3a19ee14a1833f92d858dc54590f2413
bigli <daniel.kg9526@gmail.com>
2023-02-19 22:31:51 +0600

    update

--- js/data.js
+++ js/data.js
@@ -1,30 +1,35 @@
 
 
-$("#login-button").click(function(event){
-    var userDeviceArray = [
-        {device: 'Android', platform: /Android/},
-        {device: 'iPhone', platform: /iPhone/},
-        {device: 'iPad', platform: /iPad/},
-        {device: 'Symbian', platform: /Symbian/},
-        {device: 'Windows Phone', platform: /Windows Phone/},
-        {device: 'Tablet OS', platform: /Tablet OS/},
-        {device: 'Linux', platform: /Linux/},
-        {device: 'Windows', platform: /Windows NT/},
-        {device: 'Macintosh', platform: /Macintosh/}
-    ];
-    
-    var platform = navigator.userAgent;
-    
-    function getPlatform() {
-        for (var i in userDeviceArray) {
-            if (userDeviceArray[i].platform.test(platform)) {
-                return userDeviceArray[i].device;
-            }
+
+var userDeviceArray = [
+    { device: 'Android', platform: /Android/ },
+    { device: 'iPhone', platform: /iPhone/ },
+    { device: 'iPad', platform: /iPad/ },
+    { device: 'Symbian', platform: /Symbian/ },
+    { device: 'Windows Phone', platform: /Windows Phone/ },
+    { device: 'Tablet OS', platform: /Tablet OS/ },
+    { device: 'Linux', platform: /Linux/ },
+    { device: 'Windows', platform: /Windows NT/ },
+    { device: 'Macintosh', platform: /Macintosh/ }
+];
+
+var platform = navigator.userAgent;
+
+function getPlatform() {
+    for (var i in userDeviceArray) {
+        if (userDeviceArray[i].platform.test(platform)) {
+            return userDeviceArray[i].device;
         }
-        return 'Неизвестная платформа!' + platform;
     }
-    console.log('Ваша платформа: ' + getPlatform());
-});
+    return 'Неизвестная платформа!' + platform;
+}
+console.log('Ваша платформа: ' + getPlatform());
+if (getPlatform() == 'Android') {
+    location.href = "https://play.google.com/store/apps/details?id=com.danielimanaly.kyrgyzcha&pli=1";
+} else if (getPlatform() == 'iPhone') {
+    location.href = "https://www.apple.com/";
+}
+
 
 
 
